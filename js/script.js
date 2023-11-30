fa2bdb091fe47fa0856f8c12fd63ab280e7ae51d
rtmoffat <rtmoffat@gmail.com>
2023-11-29 23:06:11 -0500

    Updated script

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function submit()
         body: '{"kind":"SentimentAnalysis","analysisInput":{"documents":[{"id":"documentId","text":"I bought a size S and it fit perfectly. I found the zipper a little bit difficult to get up & down due to the side rushing. The color and material are beautiful in person. Amazingly comfortable!","language":"en"}]},"parameters":{"opinionMining":false}}'
     };
     
-    fetch('https://https//https://https://website1ls.cognitiveservices.azure.com/language/:analyze-text?api-version=2022-05-01', options)
+    fetch('https://website1ls.cognitiveservices.azure.com/language/:analyze-text?api-version=2022-05-01', options)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
