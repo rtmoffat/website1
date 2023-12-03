321a68198be2c665ecb795e9ef4deaa179b725cf
rtmoffat <rtmoffat@gmail.com>
2023-12-02 20:57:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function submit()
 {
+    body={}
+    body['review']=document.getElementById("review").value
     const options = {
         method: 'post',
         headers: {
@@ -11,7 +13,7 @@ function submit()
         'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': 'YOUR KEY'
         },
-        body: '{review:}'
+        body: body
     };
     
     fetch('https://prod-04.eastus.logic.azure.com:443/workflows/4c1dc9a6b6744c59a313e3a85ab5af03/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=D8bYEXdBZ1zedTpSmLZ3Wmuw3bXktrnKTajAFnVQQ_4', options)
