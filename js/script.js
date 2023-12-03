7371220e5f2945b0e9b06e917c074cb37da8deee
rtmoffat <rtmoffat@gmail.com>
2023-12-02 21:09:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,9 @@ function submit()
     
     fetch('https://prod-04.eastus.logic.azure.com:443/workflows/4c1dc9a6b6744c59a313e3a85ab5af03/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=D8bYEXdBZ1zedTpSmLZ3Wmuw3bXktrnKTajAFnVQQ_4', options)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response () => {
+            console.log(response))
+            document.getElementById('response').innerHTML=response
+        }
         .catch(err => console.error(err));
 }
